7233c8139909770b990b0e51f59c8d3fe942249b
RuieX <runjiexia28@gmail.com>
2022-08-24 04:04:02 +0200

    fix: a star arrive times problem

--- a_star/a_star.py
+++ a_star/a_star.py
@@ -32,6 +32,12 @@ class Graph:
         self.timetable = timetable
         self.departure = dep_time
 
+    def is_int(self, v):
+        return isinstance(v, int)
+
+    def both_int(self, v1, v2):
+        return self.is_int(v1) and self.is_int(v2)
+
     def get_neighbors(self, v):
         return self.adjacency_list[v]
 
@@ -59,9 +65,9 @@ class Graph:
 
             # find a node with the lowest value of f() - evaluation function
             for v in open_lst:
-                if n is None or (curr_time[v] + self.heuristic[v] < curr_time[n] + self.heuristic[n]):
-                    # or (isinstance(arrival_times[v], int) and isinstance(arrival_times[n], int)
-                    #     and arrival_times[v] + self.heuristic[v] <= arrival_times[n] + self.heuristic[v])
+                if n is None \
+                        or (self.both_int(curr_time[v], curr_time[n])
+                            and curr_time[v] + self.heuristic[v] < curr_time[n] + self.heuristic[n]):
                     n = v
 
             if n is None:
@@ -73,68 +79,69 @@ class Graph:
             if n == stop:
                 reconst_path = []
                 duration = 0
-
-                tmp = []
+                times = []
 
                 while parents[n] != n:
                     reconst_path.append(self.timetable.stops.get_stop(n).name)
-                    n = parents[n]
                     duration = duration + durations[n]
-
-                    tmp.append(arrival_times[n])
+                    times.append(arrival_times[n])
+                    n = parents[n]
 
                 reconst_path.append(self.timetable.stops.get_stop(start).name)
                 reconst_path.reverse()
                 duration = duration + durations[start]
+                times.append(arrival_times[start])
+                times.reverse()
 
                 print('Path found: {}'.format(reconst_path))
                 print('duration: ', sec2str(duration))
-
-                tmp.reverse()
-                print('arrival time: ', tmp)  # todo da correggere
+                print('arrival time: ', times)
 
                 return reconst_path
 
             # for all the neighbors of the current node do
-            # Note that step is old (m, weight), m = id, weight = duration
             for step in self.get_neighbors(n):
-                # check id it's transfer and add dep and arr time --> better not
-                # if isinstance(step.departure_time, str) and isinstance(step.arrive_time, str):
-                #     step.departure_time = arrival_times[n]
-                #     step.arrive_time = arrival_times[n] + step.duration
+                # todo check waiting time
 
-                # if isinstance(step.departure_time, str) \
-                #         or (isinstance(step.departure_time, int) and isinstance(arrival_times[n], int)
-                #             and arrival_times[n] <= step.departure_time):
+                if not self.is_int(step.departure_time) \
+                        or (self.both_int(step.departure_time, arrival_times[n])
+                            and arrival_times[n] <= step.departure_time) \
+                        or (not self.is_int(arrival_times[n]) and self.departure <= step.departure_time):
 
                     # if the current node is not present in both open_lst and closed_lst
                     # add it to open_lst and note n as it's parents
                     if step.stop_to.id not in open_lst and step.stop_to.id not in closed_lst:
                         open_lst.add(step.stop_to.id)
 
+                        if self.is_int(step.arrive_time):  # this cover all hours
+                            curr_time[step.stop_to.id] = step.arrive_time  # original: curr_time[n] + step.duration
+                            arrival_times[step.stop_to.id] = step.arrive_time
+                        else:
+                            curr_time[step.stop_to.id] = curr_time[n] + step.duration
+                            arrival_times[step.stop_to.id] = curr_time[n] + step.duration
                         parents[step.stop_to.id] = n
-                        curr_time[step.stop_to.id] = curr_time[n] + step.duration
                         durations[step.stop_to.id] = step.duration
-                        arrival_times[step.stop_to.id] = step.arrive_time
 
                     # otherwise, check if it's quicker to first visit n, then m
                     # and if it is, update parents data and curr_dist data
                     # and if the node was in the closed_lst, move it to open_lst
                     else:
-                        if curr_time[step.stop_to.id] > curr_time[n] + step.duration:
+                        if (self.both_int(curr_time[step.stop_to.id], curr_time[n])
+                                and curr_time[step.stop_to.id] > curr_time[n] + step.duration):
 
+                            if self.is_int(step.arrive_time):
+                                curr_time[step.stop_to.id] = step.arrive_time
+                                arrival_times[step.stop_to.id] = step.arrive_time
+                            else:
+                                curr_time[step.stop_to.id] = curr_time[n] + step.duration
+                                arrival_times[step.stop_to.id] = curr_time[n] + step.duration
                             parents[step.stop_to.id] = n
-                            curr_time[step.stop_to.id] = curr_time[n] + step.duration
                             durations[step.stop_to.id] = step.duration
-                            arrival_times[step.stop_to.id] = step.arrive_time
+                            # todo consider to make a method instead of these 2 blocks of the same code
 
                             if step.stop_to.id in closed_lst:
                                 closed_lst.remove(step.stop_to.id)
                                 open_lst.add(step.stop_to.id)
-
-                    # todo consider to make a method instead of these 2 blocks of the same code
-                    # la durata me la calcolo alla fine prendendo inizio e fine
-                    # if step.arrive_time == "x":             to consider
 
             # remove n from the open_lst, and add it to closed_lst
             # because all of his neighbors were inspected
@@ -144,12 +151,5 @@ class Graph:
         print('Path does not exist!')
         return None
 
-# todo considerare anche tempo di partenza, tempo di arrivo, tempo corrente
-# todo aggiornare tempo di arrivo e partenza quando e1 un transfer
-    # uso il tempo come peso, bisogna calcolare i tempi "buchi" di attesa
-# todo salvare la sequenza di step fatti
-
-    # raptor normale non tiene conto del numero di cambi --> ottimizza solo il tempo, puo fare anche tanti cambi
-    # transfers ci sta il tempo di percorrenza in secondi --> devo dire all'algoritmo di considerare i transfers a piedi
-    # euristica e peso non stessa scala --> calcolo tempo di percorrenza in linea d'aria (considero una velocita media)
-
+    # todo salvare la sequenza di step fatti
+    # todo try to not use "x" ad dep_time and arr_time in transfers
